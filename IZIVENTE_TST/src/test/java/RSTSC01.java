46da46a9184fe36738f03adab0ce9f952ee0dd35
levieilfa <levieilfa@DK277176.cib.net>
2018-02-08 15:34:27 +0100

    Mise à jour du scénario RSTSC01

--- IZIVENTE_TST/src/test/java/RSTSC01.java
+++ IZIVENTE_TST/src/test/java/RSTSC01.java
@@ -1,38 +1,37 @@
 package test.java;
 
-import org.openqa.selenium.firefox.FirefoxProfile;
+import org.junit.Test;
 
-import beans.ObjectifBean;
 import exceptions.SeleniumException;
 import main.bean.CasEssaiIziventeBean;
 import main.constantes.Catalogue;
-import moteurs.FirefoxImpl;
-import moteurs.GenericDriver;
+import main.constantes.Constantes;
 import outils.SeleniumOutils;
 
-public class RSTSC01 extends SC00Test {
+public class RSTSC01 extends TNRSC00 {
 	/**
 	 * Id de sérialisation.
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	public void accesIzivente() throws SeleniumException {
+	@Test
+	public void lancementRSTSC01() throws SeleniumException {
 		// Description du scénario
 		CasEssaiIziventeBean scenario1 = new CasEssaiIziventeBean();
-		scenario1.setAlm(true);
-		scenario1.setIdUniqueTestLab(49506);
+		scenario1.setAlm(false);
+		scenario1.setIdUniqueTestLab(-1);
 		scenario1.setNomCasEssai("RSTSC01-" + getTime());
 		scenario1.setDescriptif(Catalogue.RSTSC01_TITRE);
 		scenario1.setNomTestLab(Catalogue.RSTSC01_TITRE);
-
 		scenario1.setCheminTestLab(Catalogue.CHEMIN_TEST_LAB_RST);
+		scenario1.setDistributeur(Constantes.CAS_CE);
+		scenario1.setEtablissement("17515");
+		scenario1.setAgence("1751500030000007");
 		
-		// Configuration du driver
+		/*// Configuration du driver
 		FirefoxProfile profile = configurerProfilNatixis();
-		
 		// Création et configuration du repertoire de téléchargement, ce repertoire est commun pour tous les CT du scénario
 		scenario1.setRepertoireTelechargement(creerRepertoireTelechargement(scenario1, profile));
-		
 		// Initialisation du driver
 		FirefoxImpl driver = new FirefoxImpl(profile);
 		
@@ -40,8 +39,12 @@ public class RSTSC01 extends SC00Test {
 		scenario1.ajouterObjectif(new ObjectifBean("Test arrivé à terme", scenario1.getNomCasEssai() + scenario1.getTime()));
 		
 		SeleniumOutils outil = new SeleniumOutils(driver, GenericDriver.FIREFOX_IMPL);
-		outil.setRepertoireRacine(scenario1.getRepertoireTelechargement());
+		outil.setRepertoireRacine(scenario1.getRepertoireTelechargement());*/
 		
+		SeleniumOutils outil = obtenirDriver(scenario1);
+		scenario1.getTests().add(CT01Initialisation(scenario1, outil));
+		
+		//A PARTIR D'ICI il faut se rendre dans restructuration en utilisant des ACTIONS
 		
 		// Finalisation normale du cas de test.
 		finaliserTest(outil, scenario1, scenario1.getNomCasEssai() + scenario1.getDateCreation().getTime());
